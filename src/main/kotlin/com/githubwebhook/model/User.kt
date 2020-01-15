428693848b12c428d003f9f87d6fb995f3413666
paulo <paulo-3425@hotmail.com>
2020-01-15 15:00:53 -0300

    User.kt was changed for using exposed framework

--- src/main/kotlin/com/githubwebhook/model/User.kt
+++ src/main/kotlin/com/githubwebhook/model/User.kt
@@ -1,3 +1,17 @@
 package com.githubwebhook.model
 
-data class User (val name: String, val email: String, val id: Int)
+import org.jetbrains.exposed.sql.Table
+import java.util.*
+
+data class User (
+	val name: String,
+	val email: String,
+	val id: Int
+)
+
+object UserModel: Table("users") {
+	val id = integer("id").primaryKey()
+	val name = text("name")
+	val email = text("email")
+
+}
