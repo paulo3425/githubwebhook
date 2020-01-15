8b06c7dd8748af5838a8b26a4043922f471435ec
paulo <paulo-3425@hotmail.com>
2020-01-15 14:58:04 -0300

    call database config in start method

--- src/main/kotlin/com/githubwebhook/Main.kt
+++ src/main/kotlin/com/githubwebhook/Main.kt
@@ -1,58 +1,68 @@
 package com.githubwebhook
 
-import com.githubwebhook.controller.UserController
-import io.javalin.Javalin
-import io.javalin.apibuilder.ApiBuilder.*
-import io.javalin.plugin.openapi.OpenApiOptions
-import io.javalin.plugin.openapi.OpenApiPlugin
-import io.javalin.plugin.openapi.ui.ReDocOptions
-import io.javalin.plugin.openapi.ui.SwaggerOptions
-import io.swagger.v3.oas.models.info.Info
 import com.githubwebhook.dao.UserDao
 import com.githubwebhook.model.User
+import com.githubwebhook.service.UserService
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
+import io.javalin.Javalin
+import io.javalin.apibuilder.ApiBuilder.delete
+import io.javalin.apibuilder.ApiBuilder.get
+import io.javalin.apibuilder.ApiBuilder.patch
+import io.javalin.apibuilder.ApiBuilder.post
+import org.jetbrains.exposed.sql.Database
 
 fun main() {
 
-  val userDao = UserDao()
+	fun initDB() {
+		val config = HikariConfig("/hikari.properties")
+		config.schema = "public"
+		val ds = HikariDataSource(config)
+		Database.connect(ds)
+	}
 
-    val app = Javalin.create().apply {
-        exception(Exception::class.java) { e, ctx -> e.printStackTrace() }
-        error(404) { ctx -> ctx.json("not found") }
-    }.start(7000)
+	val userService = UserService()
 
-    app.routes {
+	val app = Javalin.create().apply {
+		initDB()
+		exception(Exception::class.java) { e, ctx -> e.printStackTrace() }
+		error(404) { ctx -> ctx.json("not found") }
+	}.start(7000)
 
-        get("/users") { ctx ->
-            ctx.json(userDao.users)
-        }
+	app.routes {
 
-        get("/users/:user-id") { ctx ->
-            ctx.json(userDao.findById(ctx.pathParam("user-id").toInt())!!)
-        }
 
-        get("/users/email/:email") { ctx ->
-            ctx.json(userDao.findByEmail(ctx.pathParam("email"))!!)
-        }
+		get("/users") { ctx ->
+			ctx.json(userService.getAll())
+		}
 
-        post("/users") { ctx ->
-            val user = ctx.body<User>()
-            userDao.save(name = user.name, email = user.email)
-            ctx.status(201)
-        }
+//		get("/users/:user-id") { ctx ->
+//			ctx.json(userDao.findById(ctx.pathParam("user-id").toInt())!!)
+//		}
+//
+//		get("/users/email/:email") { ctx ->
+//			ctx.json(userDao.findByEmail(ctx.pathParam("email"))!!)
+//		}
+//
+//		post("/users") { ctx ->
+//			val user = ctx.body<User>()
+//			userDao.save(name = user.name, email = user.email)
+//			ctx.status(201)
+//		}
+//
+//		patch("/users/:user-id") { ctx ->
+//			val user = ctx.body<User>()
+//			userDao.update(
+//				id = ctx.pathParam("user-id").toInt(),
+//				user = user
+//			)
+//			ctx.status(204)
+//		}
+//
+//		delete("/users/:user-id") { ctx ->
+//			userDao.delete(ctx.pathParam("user-id").toInt())
+//			ctx.status(204)
+//		}
 
-        patch("/users/:user-id") { ctx ->
-            val user = ctx.body<User>()
-            userDao.update(
-                    id = ctx.pathParam("user-id").toInt(),
-                    user = user
-            )
-            ctx.status(204)
-        }
-
-        delete("/users/:user-id") { ctx ->
-            userDao.delete(ctx.pathParam("user-id").toInt())
-            ctx.status(204)
-        }
-
-    }
+	}
 }
